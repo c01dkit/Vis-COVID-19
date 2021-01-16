0cd2bcedb2056eb0fe9aa776bb91090b270f14e3
BoyuChang <bychang1801@gmail.com>
2021-01-17 01:59:52 +0800

    动态更新基本完成，除了右下角需要单独做数据源；雷达图更新会导致卡顿，估计还有很多要改的

--- worldmap.js
+++ worldmap.js
@@ -2,52 +2,50 @@
 // 全局变量与状态保存
 const oneDay = 24 * 3600 * 1000; // 24h毫秒数 用于计算日期偏移
 const base = +new Date(2020, 2, 1);
-var fileName = "./data/2020-2-1.json" // 预加载文件
-var previousProgressBarValue = null // 置空进度条数值
-var previousSelector = null  // 置空下拉列表值
-
+var fileName = "./data/2020-2-1.json"; // 预加载文件
+var previousProgressBarValue = null; // 置空进度条数值
+var previousSelector = null;  // 置空下拉列表值
+const caseType = ['累计确诊','新增确诊','累计死亡','新增死亡','累计治愈','仍在治疗','重症病例']
+const caseTypeEN = ['total_diagnosed','new_diagnosed','total_death','new_death','total_healed','still_healing','seriously_ill']
 // 左上全球数据
-var globalData = null  // 置空左上全球数据
+var globalData = null;  // 置空左上全球数据
 
 // 左下国家对比图
-var countryCasesType = "----"
-var country = ["美国", "中国", "法国", "日本", "韩国", "俄罗斯", "冰岛", "西班牙"];
-var data = [223, 312, 178, 398, 280, 112, 332, 99];
-var data_x = [];
-var data_y = [];
-var i = 0;
-while (country[i] && data[i]) {
-    data_x.push(data[i]);
-    data_y.push(country[i] + ' ' + data[i]);
-    i++;
-}
+const maxNum = 5;  // 设定最大排名数目
+let countryCasesType = "----";
+var countryCasesDataX = [];
+var countryCasesDataY = [];
 
 // 中部地图数据
-var dataList = [] // 置空中间地图数据
+var mapDataList = [] // 置空中间地图数据
+
+// 右上雷达图
+var radarDataList = [] // 置空右上雷达图数据
+var radarCountryName = "----"
+var radarMaxValue = [] // 右上雷达图最大值限定
 
 // 右下中美对比
-var xAxisTime = []; //时间轴
+const contrastTitle = '中美对比'
+var contrastXAxisTime = []; //时间轴
 var china_data = []; //中国累计确诊
 var us_data = []; //美国累计确诊
 // 加入数据后删除此循环
-for (var i = 0; i < 100; i++) {
-    xAxisTime.push('Date' + i);
+// for (var i = 0; i < 100; i++) {
+//     contrastXAxisTime.push('Date' + i);
+//     china_data.push((Math.sin(i / 5) * (i / 5 - 10) + i / 6) * 5);
+//     us_data.push((Math.cos(i / 5) * (i / 5 - 10) + i / 6) * 5);
+// }
+
+for (let i = 0 ; i < 144; i++){
+    let now = new Date(base+oneDay*i);
+    const fileName = [now.getFullYear(), now.getMonth(), now.getDate()].join('-');
+    contrastXAxisTime.push(fileName.replaceAll("-","/"));
     china_data.push((Math.sin(i / 5) * (i / 5 - 10) + i / 6) * 5);
     us_data.push((Math.cos(i / 5) * (i / 5 - 10) + i / 6) * 5);
 }
 
-var data = [
-    { country: '美国', data: 223 },
-    { country: '中国', data: 312 },
-    { country: '法国', data: 178 },
-    { country: '日本', data: 398 },
-    { country: '韩国', data: 280 },
-    { country: '俄罗斯', data: 112 },
-    { country: '冰岛', data: 332 },
-    { country: '西班牙', data: 99 },
-]
 
-function checkIfUpdated() {
+function checkIfUpdated(compulsory) {
     let needUpdate = false;
     const dateDisp = parseInt($("#progress").val())
     if (dateDisp !== previousProgressBarValue || previousProgressBarValue == null) {
@@ -59,7 +57,7 @@ function checkIfUpdated() {
         previousSelector = selector
         needUpdate = true
     }
-    if (needUpdate) {
+    if (needUpdate || compulsory) {
         const now = new Date(base + oneDay * dateDisp);
         const name = [now.getFullYear(), now.getMonth(), now.getDate()].join('-');
         refreshAll(name, selector)
@@ -70,38 +68,54 @@ function refreshAll(dateName, selector) {
     if (dateName !== null) {
         fileName = "./data/" + dateName + ".json";
     }
+
     $.get(fileName).fail(function(dataSet) {
         let resultData = eval("(" + dataSet.responseText + ")");
         let finalSets = resultData.inner_value;
         globalData = finalSets[0];
-        dataList.splice(0, dataList.length);
-        if (selector === "new_diagnosed") { // 加载新增确诊病例数据
-            for (item of finalSets) {
-                dataList.push({ name: item.region, value: item.new_diagnosed });
-            }
-        } else if (selector === "total_diagnosed") { // 加载累计确诊病例数据
-            for (item of finalSets) {
-                dataList.push({ name: item.region, value: item.total_diagnosed });
-            }
-        } else if (selector === "total_death") { // 加载累计死亡病例数据
-            for (item of finalSets) {
-                dataList.push({ name: item.region, value: item.total_death });
-            }
-        } else if (selector === "new_death") { // 加载新增死亡病例数据
-            for (item of finalSets) {
-                dataList.push({ name: item.region, value: item.new_death });
-            }
-        } else if (selector === "total_healed") { // 加载累计治愈数据
-            for (item of finalSets) {
-                dataList.push({ name: item.region, value: item.total_healed });
-            }
-        } else if (selector === "still_healing") { // 加载仍处于治疗阶段数据
-            for (item of finalSets) {
-                dataList.push({ name: item.region, value: item.still_healing });
-            }
-        } else if (selector === "seriously_ill") { // 加载病危数据
-            for (item of finalSets) {
-                dataList.push({ name: item.region, value: item.seriously_ill });
+        mapDataList.splice(0, mapDataList.length);
+        countryCasesDataY.splice(0,countryCasesDataY.length);
+        countryCasesDataX.splice(0,countryCasesDataX.length);
+        radarDataList.splice(0,radarDataList.length);
+        for (let i = 0; i < caseTypeEN.length; i++){
+            if (selector === caseTypeEN[i]){
+                let j = 0;
+                for (item of finalSets){
+                    let tempName =  item.region;
+                    let tempValue = eval("item."+caseTypeEN[i]);
+                    // 更新中部全球地图
+                    mapDataList.push({ name: tempName, value: tempValue});
+                    // 更新左下国家排名图
+                    j++;
+                    if (j < maxNum && j > 1){ // 跳过全球数据
+                        countryCasesDataX.push(tempValue);
+                        countryCasesDataY.push(tempName);
+                    }
+                    // 更新右上雷达图
+                    if (tempName === radarCountryName){
+                        radarDataList.push([
+                            eval("item."+caseTypeEN[0]),
+                            eval("item."+caseTypeEN[1]),
+                            eval("item."+caseTypeEN[2]),
+                            eval("item."+caseTypeEN[3]),
+                            eval("item."+caseTypeEN[4]),
+                            eval("item."+caseTypeEN[5]),
+                            eval("item."+caseTypeEN[6]),
+                        ])
+                        //TODO: 这里雷达图的最大值姑且这样用了。 最大值怎么确定合适呢？ 而且雷达图如果堆叠的话需要请求多个json，数据源还需要处理。
+
+                        radarMaxValue.push([
+                            eval("item."+caseTypeEN[0])+200,
+                            eval("item."+caseTypeEN[1])+200,
+                            eval("item."+caseTypeEN[2])+200,
+                            eval("item."+caseTypeEN[3])+200,
+                            eval("item."+caseTypeEN[4])+200,
+                            eval("item."+caseTypeEN[5])+200,
+                            eval("item."+caseTypeEN[6])+200,
+                        ])
+                    }
+                }
+                break;
             }
         }
         updateTextBySelector(dateName)
@@ -114,37 +128,14 @@ function refreshAll(dateName, selector) {
 
 // 根据下拉框重绘所有文字部分
 function updateTextBySelector(param) {
-
-    if (previousSelector === "new_diagnosed") {
-        $("#globaldata").text(globalData.new_diagnosed)
-        $("#gloabaltype").text(param + " 全球新增确诊")
-        countryCasesType = "新增确诊"
-    } else if (previousSelector === "total_diagnosed") { // 加载累计确诊病例数据
-        $("#globaldata").text(globalData.total_diagnosed)
-        $("#gloabaltype").text(param + " 全球累计确诊")
-        countryCasesType = "累计确诊"
-    } else if (previousSelector === "total_death") { // 加载累计死亡病例数据
-        $("#globaldata").text(globalData.total_death)
-        $("#gloabaltype").text(param + " 全球累计死亡")
-        countryCasesType = "累计死亡"
-    } else if (previousSelector === "new_death") { // 加载新增死亡病例数据
-        $("#globaldata").text(globalData.new_death)
-        $("#gloabaltype").text(param + " 全球新增死亡")
-        countryCasesType = "新增死亡"
-    } else if (previousSelector === "total_healed") { // 加载累计治愈数据
-        $("#globaldata").text(globalData.total_healed)
-        $("#gloabaltype").text(param + " 全球累计治愈")
-        countryCasesType = "累计治愈"
-    } else if (previousSelector === "still_healing") { // 加载仍处于治疗阶段数据
-        $("#globaldata").text(globalData.still_healing)
-        $("#gloabaltype").text(param + " 全球仍在治疗")
-        countryCasesType = "仍在治疗"
-    } else if (previousSelector === "seriously_ill") { // 加载病危数据
-        $("#globaldata").text(globalData.seriously_ill)
-        $("#gloabaltype").text(param + " 全球重症病例")
-        countryCasesType = "重症病例"
+    for (var i = 0 ; i < caseTypeEN.length; i++){
+        if (previousSelector === caseTypeEN[i]){
+            $("#globaldata").text(eval("globalData."+caseTypeEN[i]))
+            $("#gloabaltype").text(param + " 全球"+caseType[i])
+            countryCasesType = caseType[i]
+            break
+        }
     }
-
 }
 
 // 重绘左下国家对比图
@@ -152,11 +143,6 @@ function drawLeftBottomCountryCases() {
     var left2 = echarts.init(document.getElementById('left2'));
     var option = {
         color: '#f02512',
-
-        dataset: {
-            source: data,
-        },
-
         grid: {
             width: '85%',
             left: '8%',
@@ -167,7 +153,7 @@ function drawLeftBottomCountryCases() {
         },
 
         xAxis: [{
-            data: data_x,
+            data: countryCasesDataX,
             show: false,
             type: 'value',
             name: 'Count',
@@ -176,7 +162,7 @@ function drawLeftBottomCountryCases() {
 
         }],
         yAxis: [{
-            data: data_y,
+            data: countryCasesDataY,
             type: "category",
             //"name": "Country",
             //是否反向坐标轴
@@ -238,7 +224,7 @@ function drawLeftBottomCountryCases() {
             type: "bar",
             name: countryCasesType,
             // "data": data_x,
-            data: data_x.sort(function(a, b) {
+            data: countryCasesDataX.sort(function(a, b) {
                 return b - a;
             }),
             itemStyle: {
@@ -377,120 +363,20 @@ function drawMiddleMap() {
                 name: '疫情人数',
                 type: 'map', //图表类型
                 geoIndex: 0,
-                data: dataList //图表的数据
+                data: mapDataList //图表的数据
             }
         ]
     }
     map.setOption(option); //用配置项配置（动词）echarts
+    map.on('click',function (param){
+        radarCountryName = param.name
+        checkIfUpdated(true)
+    })
 }
 
 // 重绘右上雷达图
 function drawRightTopRadar() {
     var radar = echarts.init(document.getElementById('right12')); //初始化
-
-    // Schema:
-    // date,AQIindex,PM2.5,PM10,CO,NO2,SO2
-    var dataBJ = [
-        [55, 9, 56, 0.46, 18, 6, 1],
-        [25, 11, 21, 0.65, 34, 9, 2],
-        [56, 7, 63, 0.3, 14, 5, 3],
-        [33, 7, 29, 0.33, 16, 6, 4],
-        [42, 24, 44, 0.76, 40, 16, 5],
-        [82, 58, 90, 1.77, 68, 33, 6],
-        [74, 49, 77, 1.46, 48, 27, 7],
-        [78, 55, 80, 1.29, 59, 29, 8],
-        [267, 216, 280, 4.8, 108, 64, 9],
-        [185, 127, 216, 2.52, 61, 27, 10],
-        [39, 19, 38, 0.57, 31, 15, 11],
-        [41, 11, 40, 0.43, 21, 7, 12],
-        [64, 38, 74, 1.04, 46, 22, 13],
-        [108, 79, 120, 1.7, 75, 41, 14],
-        [108, 63, 116, 1.48, 44, 26, 15],
-        [33, 6, 29, 0.34, 13, 5, 16],
-        [94, 66, 110, 1.54, 62, 31, 17],
-        [186, 142, 192, 3.88, 93, 79, 18],
-        [57, 31, 54, 0.96, 32, 14, 19],
-        [22, 8, 17, 0.48, 23, 10, 20],
-        [39, 15, 36, 0.61, 29, 13, 21],
-        [94, 69, 114, 2.08, 73, 39, 22],
-        [99, 73, 110, 2.43, 76, 48, 23],
-        [31, 12, 30, 0.5, 32, 16, 24],
-        [42, 27, 43, 1, 53, 22, 25],
-        [154, 117, 157, 3.05, 92, 58, 26],
-        [234, 185, 230, 4.09, 123, 69, 27],
-        [160, 120, 186, 2.77, 91, 50, 28],
-        [134, 96, 165, 2.76, 83, 41, 29],
-        [52, 24, 60, 1.03, 50, 21, 30],
-        [46, 5, 49, 0.28, 10, 6, 31]
-    ];
-
-    var dataGZ = [
-        [26, 37, 27, 1.163, 27, 13, 1],
-        [85, 62, 71, 1.195, 60, 8, 2],
-        [78, 38, 74, 1.363, 37, 7, 3],
-        [21, 21, 36, 0.634, 40, 9, 4],
-        [41, 42, 46, 0.915, 81, 13, 5],
-        [56, 52, 69, 1.067, 92, 16, 6],
-        [64, 30, 28, 0.924, 51, 2, 7],
-        [55, 48, 74, 1.236, 75, 26, 8],
-        [76, 85, 113, 1.237, 114, 27, 9],
-        [91, 81, 104, 1.041, 56, 40, 10],
-        [84, 39, 60, 0.964, 25, 11, 11],
-        [64, 51, 101, 0.862, 58, 23, 12],
-        [70, 69, 120, 1.198, 65, 36, 13],
-        [77, 105, 178, 2.549, 64, 16, 14],
-        [109, 68, 87, 0.996, 74, 29, 15],
-        [73, 68, 97, 0.905, 51, 34, 16],
-        [54, 27, 47, 0.592, 53, 12, 17],
-        [51, 61, 97, 0.811, 65, 19, 18],
-        [91, 71, 121, 1.374, 43, 18, 19],
-        [73, 102, 182, 2.787, 44, 19, 20],
-        [73, 50, 76, 0.717, 31, 20, 21],
-        [84, 94, 140, 2.238, 68, 18, 22],
-        [93, 77, 104, 1.165, 53, 7, 23],
-        [99, 130, 227, 3.97, 55, 15, 24],
-        [146, 84, 139, 1.094, 40, 17, 25],
-        [113, 108, 137, 1.481, 48, 15, 26],
-        [81, 48, 62, 1.619, 26, 3, 27],
-        [56, 48, 68, 1.336, 37, 9, 28],
-        [82, 92, 174, 3.29, 0, 13, 29],
-        [106, 116, 188, 3.628, 101, 16, 30],
-        [118, 50, 0, 1.383, 76, 11, 31]
-    ];
-
-    var dataSH = [
-        [91, 45, 125, 0.82, 34, 23, 1],
-        [65, 27, 78, 0.86, 45, 29, 2],
-        [83, 60, 84, 1.09, 73, 27, 3],
-        [109, 81, 121, 1.28, 68, 51, 4],
-        [106, 77, 114, 1.07, 55, 51, 5],
-        [109, 81, 121, 1.28, 68, 51, 6],
-        [106, 77, 114, 1.07, 55, 51, 7],
-        [89, 65, 78, 0.86, 51, 26, 8],
-        [53, 33, 47, 0.64, 50, 17, 9],
-        [80, 55, 80, 1.01, 75, 24, 10],
-        [117, 81, 124, 1.03, 45, 24, 11],
-        [99, 71, 142, 1.1, 62, 42, 12],
-        [95, 69, 130, 1.28, 74, 50, 13],
-        [116, 87, 131, 1.47, 84, 40, 14],
-        [108, 80, 121, 1.3, 85, 37, 15],
-        [134, 83, 167, 1.16, 57, 43, 16],
-        [79, 43, 107, 1.05, 59, 37, 17],
-        [71, 46, 89, 0.86, 64, 25, 18],
-        [97, 71, 113, 1.17, 88, 31, 19],
-        [84, 57, 91, 0.85, 55, 31, 20],
-        [87, 63, 101, 0.9, 56, 41, 21],
-        [104, 77, 119, 1.09, 73, 48, 22],
-        [87, 62, 100, 1, 72, 28, 23],
-        [168, 128, 172, 1.49, 97, 56, 24],
-        [65, 45, 51, 0.74, 39, 17, 25],
-        [39, 24, 38, 0.61, 47, 17, 26],
-        [39, 24, 39, 0.59, 50, 19, 27],
-        [93, 68, 96, 1.05, 79, 29, 28],
-        [188, 143, 197, 1.66, 99, 51, 29],
-        [174, 131, 174, 1.55, 108, 50, 30],
-        [187, 143, 201, 1.39, 89, 53, 31]
-    ];
 
     var lineStyle = {
         normal: {
@@ -502,7 +388,7 @@ function drawRightTopRadar() {
     option = {
         backgroundColor: '#222222',
         title: {
-            text: 'Single Country',
+            text: radarCountryName,
             left: 10,
             top: 10,
             textStyle: {
@@ -512,31 +398,22 @@ function drawRightTopRadar() {
         },
         legend: {
             bottom: 5,
-            data: ['China', 'US', 'Korea'],
+            data: [radarCountryName],
             itemGap: 20,
             textStyle: {
                 color: '#d6d6d6',
                 fontSize: 12
             },
-            // selectedMode: 'single'
         },
-        // visualMap: {
-        //     show: true,
-        //     min: 0,
-        //     max: 20,
-        //     dimension: 6,
-        //     inRange: {
-        //         colorLightness: [0.5, 0.8]
-        //     }
-        // },
         radar: {
             indicator: [
-                { name: 'NC', max: 300 },
-                { name: 'CC', max: 300 },
-                { name: 'ND', max: 300 },
-                { name: 'CD', max: 5 },
-                { name: 'UT', max: 200 },
-                { name: 'CR', max: 100 }
+                { name: caseType[0], max: radarMaxValue[0] },
+                { name: caseType[1], max: radarMaxValue[1] },
+                { name: caseType[2], max: radarMaxValue[2] },
+                { name: caseType[3], max: radarMaxValue[3] },
+                { name: caseType[4], max: radarMaxValue[4] },
+                { name: caseType[5], max: radarMaxValue[5] },
+                { name: caseType[6], max: radarMaxValue[6] },
             ],
             shape: 'circle',
             splitNumber: 5,
@@ -564,10 +441,10 @@ function drawRightTopRadar() {
             }
         },
         series: [{
-            name: 'China',
+            name: radarCountryName,
             type: 'radar',
             lineStyle: lineStyle,
-            data: dataBJ,
+            data: radarDataList,
             symbol: 'none',
             itemStyle: {
                 color: '#F9713C'
@@ -576,32 +453,6 @@ function drawRightTopRadar() {
                 opacity: 0.1
             }
         },
-            {
-                name: 'US',
-                type: 'radar',
-                lineStyle: lineStyle,
-                data: dataSH,
-                symbol: 'none',
-                itemStyle: {
-                    color: '#B3E4A1'
-                },
-                areaStyle: {
-                    opacity: 0.05
-                }
-            },
-            {
-                name: 'Korea',
-                type: 'radar',
-                lineStyle: lineStyle,
-                data: dataGZ,
-                symbol: 'none',
-                itemStyle: {
-                    color: 'rgb(238, 197, 102)'
-                },
-                areaStyle: {
-                    opacity: 0.05
-                }
-            }
         ]
     };
 
@@ -615,7 +466,7 @@ function drawRightBottomContrast() {
 
     option = {
         title: {
-            text: 'China vs US for Cumulative Cases',
+            text: contrastTitle,
             top: 220,
             left: 80,
             align: 'left',
@@ -625,7 +476,7 @@ function drawRightBottomContrast() {
             },
         },
         legend: {
-            data: ['CHINA', 'US'],
+            data: ['中国', '美国'],
             show: true,
             top: 15,
             align: 'left',
@@ -648,7 +499,7 @@ function drawRightBottomContrast() {
         },
         tooltip: {},
         xAxis: {
-            data: xAxisTime,
+            data: contrastXAxisTime,
             splitLine: {
                 show: false
             },
@@ -692,7 +543,7 @@ function drawRightBottomContrast() {
             //showDetail: false
         }],
         series: [{
-            name: 'CHINA',
+            name: '中国',
             type: 'bar',
             color: 'red',
 
@@ -701,7 +552,7 @@ function drawRightBottomContrast() {
                 return idx * 10;
             }
         }, {
-            name: 'US',
+            name: '美国',
             type: 'bar',
             color: '#1d3c95',
             data: us_data,
